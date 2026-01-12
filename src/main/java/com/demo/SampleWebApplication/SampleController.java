1a7f874891f7b21d9e596bffd63ceb9a0ed62f0c
gitTestuser1107 <mujibrahman.aydin@gmail.com>
2026-01-12 19:09:51 +0530

    Update env yml

--- src/main/java/com/demo/SampleWebApplication/SampleController.java
+++ src/main/java/com/demo/SampleWebApplication/SampleController.java
@@ -1,14 +1,18 @@
 package com.demo.SampleWebApplication;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
-
+	
+	@Value("${data.from}")
+	private String dataFrom;
+	
 	@GetMapping
 	public String getSampleResponse() {
-		return "Welcome to the world of Spring Boot";
+		return "Welcome to the world of Spring Boot from " + dataFrom;
 	}
 
 }
